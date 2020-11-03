82c1451af2caa74bf4340ce477acda73c548ddfd
ErikGraciosa <erikgraciosa@gmail.com>
2020-11-03 08:06:34 -0800

    heroku deploy

---
+++ data/load-seed-data.js
@@ -0,0 +1,47 @@
+const client = require('../lib/client');
+// import our seed data:
+const pinball_machines = require('./pinball_machines.js');
+const usersData = require('./users.js');
+const { getEmoji } = require('../lib/emoji.js');
+
+run();
+
+async function run() {
+
+  try {
+    await client.connect();
+
+    const users = await Promise.all(
+      usersData.map(user => {
+        return client.query(`
+                      INSERT INTO users (email, hash)
+                      VALUES ($1, $2)
+                      RETURNING *;
+                  `,
+        [user.email, user.hash]);
+      })
+    );
+      
+    const user = users[0].rows[0];
+
+    await Promise.all(
+      pinball_machines.map(pinball => {
+        return client.query(`
+                    INSERT INTO pinball_machines (name, year_manufactured, multiball, owner_id)
+                    VALUES ($1, $2, $3, $4);
+                `,
+        [pinball.name, pinball.year_manufactured, pinball.multiball, user.id]);
+      })
+    );
+    
+
+    console.log('seed data load complete', getEmoji(), getEmoji(), getEmoji());
+  }
+  catch(err) {
+    console.log(err);
+  }
+  finally {
+    client.end();
+  }
+    
+}
---
+++ data/pinball_machines.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    name: 'Skateball',
+    year_manufactured: 1974,
+    multiball: false,
+  },
+  {
+    name: 'Firepower',
+    year_manufactured: 1980,
+    multiball: true,
+  },
+  {
+    name: 'The Addams Family',
+    year_manufactured: 1994,
+    multiball: true,
+  },
+  {
+    name: 'World Cup Soccer "94"',
+    year_manufactured: 1994,
+    multiball: true,
+  },
+  {
+    name: 'Fish Tales',
+    year_manufactured: 1992,
+    multiball: true,
+  }
+];
