ac836d8a8afbed91bdbadf85bc97a2344200740a
ErikGraciosa <erikgraciosa@gmail.com>
2020-11-04 09:51:18 -0800

    TDD of post test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,19 +8,19 @@ const client = require('../lib/client');
 
 describe('app routes', () => {
   describe('routes', () => {
-    let token;
+    // let token;
   
     beforeAll(async done => {
       execSync('npm run setup-db');
   
       client.connect();
   
-      const signInData = await fakeRequest(app)
-        .post('/auth/signup')
-        .send({
-          email: 'jon@user.com',
-          password: '1234'
-        });
+      // const signInData = await fakeRequest(app)
+      //   .post('/auth/signup')
+      //   .send({
+      //     email: 'jon@user.com',
+      //     password: '1234'
+      //   });
       
       // token = signInData.body.token;
   
@@ -57,7 +57,7 @@ describe('app routes', () => {
         },
         {
           'id': 4,
-          'name': 'World Cup Soccer "94"',
+          'name': 'World Cup Soccer 94',
           'year_manufactured': 1994,
           'multiball': true,
           'owner_id': 1
@@ -78,5 +78,75 @@ describe('app routes', () => {
 
       expect(data.body).toEqual(expectation);
     });
+
+
+    test('returns pinball_machines after post sent to confirm added entry', async() => {
+
+      const expectation = [
+        {
+          'id': 1,
+          'name': 'Skateball',
+          'year_manufactured': 1974,
+          'multiball': false,
+          'owner_id': 1
+        },
+        {
+          'id': 2,          
+          'name': 'Firepower',          
+          'year_manufactured': 1980,
+          'multiball': true,
+          'owner_id': 1       
+        },
+        {
+          'id': 3,
+          'name': 'The Addams Family',
+          'year_manufactured': 1994,
+          'multiball': true,
+          'owner_id': 1
+        },
+        {
+          'id': 4,
+          'name': 'World Cup Soccer 94',
+          'year_manufactured': 1994,
+          'multiball': true,
+          'owner_id': 1
+        },
+        {
+          'id': 5,
+          'name': 'Fish Tales',
+          'year_manufactured': 1992,
+          'multiball': true,
+          'owner_id': 1
+        },
+        {
+          'id': 6,
+          'name': 'Paragon',
+          'year_manufactured': 1976,
+          'multiball': false,
+          'owner_id': 1
+        },
+      ];
+
+      await fakeRequest(app)
+        .post('/pinball_machines')
+        .send({
+          'name': 'Paragon',
+          'year_manufactured': 1976,
+          'multiball': false,
+          'owner_id': 1
+        })
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      const data = await fakeRequest(app)
+        .get('/pinball_machines')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+
+
+
   });
 });
